fe467f1134ba8d46a1c2b72e81b336df26bb234d
a943356255 <943356255@qq.com>
2023-05-04 14:22:45 +0800

    feat: 2106. 摘水果

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain6.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain6.java
@@ -1,15 +1,65 @@
 package com.example.springboot_vue.leetcode;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class LeetCodeMain6 {
 
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("abc");
-        Stack<String> stack = new Stack<>();
-        stack.push(stringBuilder.substring(0, 0) + stringBuilder.substring(3, 3));
-        System.out.println(stack.peek().equals(""));
-//        System.out.println(stringBuilder.substring(0, 0) + stringBuilder.substring(3, 3));
+
+    }
+
+    // 2106. 摘水果
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int[] arr = new int[fruits[fruits.length - 1][0] + 1];
+        int index;
+        if (fruits[0][0] == 0) {
+            arr[0] = fruits[0][1];
+            index = 1;
+        } else {
+            arr[0] = 0;
+            index = 0;
+        }
+        // 求前缀和
+        for (int i = 1; i < arr.length; i++) {
+            if (i == fruits[index][0]) {
+                arr[i] = arr[i - 1] + fruits[index][1];
+                index++;
+            } else {
+                arr[i] = arr[i - 1];
+            }
+        }
+        int maxLeft = Math.max(0, startPos - k);
+        int maxRight = Math.min(arr.length - 1, startPos + k);
+
+        int legalStartPos;
+        if (startPos > arr.length) {
+            legalStartPos = Math.max(startPos - k, arr.length - 1);
+            k = k - (startPos - legalStartPos);
+            if (legalStartPos > arr.length) {
+                return 0;
+            }
+        } else {
+            legalStartPos = startPos;
+        }
+
+        int max, val = arr[maxRight] - arr[Math.max(Math.min(startPos - 1, maxRight), 0)];
+        if (maxLeft == 0) {
+            max = Math.max(arr[legalStartPos], val);
+        } else {
+            max = Math.max(arr[legalStartPos] - arr[maxLeft - 1], val);
+        }
+        // 优先往左走，然后右拐
+        for (int i = maxLeft + 1; i < legalStartPos; i++) {
+            max = Math.max(max, arr[Math.max(legalStartPos, maxRight - 2 * (i - maxLeft))] - arr[i - 1]);
+        }
+
+        // 优先往右走，然后左拐
+        for (int i = legalStartPos + 1; i <= maxRight; i++) {
+            max = Math.max(max, arr[i] - arr[Math.max(Math.min(startPos - k + (i - legalStartPos) * 2, legalStartPos) - 1, 0)]);
+        }
+
+        return max;
     }
 
     // 1003. 检查替换后的词是否有效
