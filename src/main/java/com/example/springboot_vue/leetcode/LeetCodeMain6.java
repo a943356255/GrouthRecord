a121da13f78256d498d4e78d04567e1391f4f495
a943356255 <943356255@qq.com>
2023-05-09 09:33:27 +0800

    fix: 2437. 有效时间的数目

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain6.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain6.java
@@ -8,6 +8,40 @@ public class LeetCodeMain6 {
 
     }
 
+    // 2437. 有效时间的数目
+    public int countTime(String time) {
+        int countHour = 0;
+        int countMinute = 0;
+        for (int i = 0; i < 24; i++) {
+            int hiHour = i / 10;
+            int loHour = i % 10;
+            if ((time.charAt(0) == '?' || time.charAt(0) == hiHour + '0') &&
+                    (time.charAt(1) == '?' || time.charAt(1) == loHour + '0')) {
+                countHour++;
+            }
+        }
+        for (int i = 0; i < 60; i++) {
+            int hiMinute = i / 10;
+            int loMinute = i % 10;
+            if ((time.charAt(3) == '?' || time.charAt(3) == hiMinute + '0') &&
+                    (time.charAt(4) == '?' || time.charAt(4) == loMinute + '0')) {
+                countMinute++;
+            }
+        }
+        return countHour * countMinute;
+    }
+
+    // 30. 串联所有单词的子串
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            Map<String, Integer> map = new HashMap<>();
+        }
+
+
+        return res;
+    }
+
     // 1010. 总持续时间可被 60 整除的歌曲 (超时)
     public int numPairsDivisibleBy60(int[] time) {
         Map<Integer, List<Integer>> map = new HashMap<>();
