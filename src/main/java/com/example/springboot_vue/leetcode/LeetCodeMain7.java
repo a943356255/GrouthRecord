dd094bad4eb0cef4c5bbb88cbcf387a03c4b763e
a943356255 <943356255@qq.com>
2023-06-11 11:13:21 +0800

    feat: 1171. 从链表中删去总和值为零的连续节点

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
@@ -8,6 +8,29 @@ public class LeetCodeMain7 {
 
     }
 
+    // 1171. 从链表中删去总和值为零的连续节点
+    public ListNode removeZeroSumSublists(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        Map<Integer, ListNode> seen = new HashMap<>();
+        int prefix = 0;
+        for (ListNode node = dummy; node != null; node = node.next) {
+            prefix += node.val;
+            // 前缀和，以及当前节点的值
+            seen.put(prefix, node);
+        }
+
+        prefix = 0;
+        for (ListNode node = dummy; node != null; node = node.next) {
+            prefix += node.val;
+            // 这里修改了节点的后继节点，会影响dummy的结构
+            node.next = seen.get(prefix).next;
+        }
+
+        return dummy.next;
+    }
+
     // 1170. 比较字符串最小字母出现频次
     public int[] numSmallerByFrequency(String[] queries, String[] words) {
         int[] count = new int[12];
