25cfbf4205bfccab8993a39d0e7f5fc3e851c449
a943356255 <943356255@qq.com>
2023-06-12 09:51:25 +0800

    feat: 102. 二叉树的层序遍历

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain6.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain6.java
@@ -101,17 +101,25 @@ public class LeetCodeMain6 {
     // 102. 二叉树的层序遍历
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
 
         Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
-        List<Integer> list = new ArrayList<>();
-        list.add(root.val);
-
+        if (root != null) {
+            queue.add(root);
+        }
         while (!queue.isEmpty()) {
-
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
         }
 
         return res;
