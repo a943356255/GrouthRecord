05335894f0ba45b0b058954801616ef44b41d926
a943356255 <943356255@qq.com>
2023-06-16 09:51:49 +0800

    feat: 200. 岛屿数量

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
@@ -10,6 +10,44 @@ public class LeetCodeMain7 {
         System.out.println(str.toString());
     }
 
+    // 200. 岛屿数量
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    void dfs(char[][] grid, int r, int c) {
+        // 判断 base case
+        if (!inArea(grid, r, c)) {
+            return;
+        }
+        // 如果这个格子不是岛屿，直接返回
+        if (grid[r][c] != '1') {
+            return;
+        }
+        grid[r][c] = '2'; // 将格子标记为「已遍历过」
+
+        // 访问上、下、左、右四个相邻结点
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    // 判断坐标 (r, c) 是否在网格中
+    boolean inArea(char[][] grid, int r, int c) {
+        return 0 <= r && r < grid.length && 0 <= c && c < grid[0].length;
+    }
+
     // 1177. 构建回文串检测
     public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
         int[] arr = new int[s.length() + 1];
