de0797e2b6cc7cf246b2eacda31b5230fc6288fb
a943356255 <943356255@qq.com>
2023-06-20 11:26:44 +0800

    feat: 226. 翻转二叉树

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
@@ -8,6 +8,42 @@ public class LeetCodeMain7 {
 
     }
 
+    // 226. 翻转二叉树
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        // 将二叉树中的节点逐层放入队列中，再迭代处理队列中的元素
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            // 每次都从队列中拿一个节点，并交换这个节点的左右子树
+            TreeNode tmp = queue.poll();
+            // 这一步交换了左右子树
+            TreeNode left = tmp.left;
+            tmp.left = tmp.right;
+            tmp.right = left;
+
+            // 如果当前节点的左子树不为空，则放入队列等待后续处理
+            if (tmp.left != null) {
+                queue.add(tmp.left);
+            }
+
+            // 如果当前节点的右子树不为空，则放入队列等待后续处理
+            if (tmp.right != null) {
+                queue.add(tmp.right);
+            }
+        }
+
+        //返回处理完的根节点
+        return root;
+    }
+
+    // 1262. 可被三整除的最大和
+    public int maxSumDivThree(int[] nums) {
+        return 0;
+    }
+
     // 1254. 统计封闭岛屿的数目
     public int closedIsland(int[][] grid) {
         int res = 0;
