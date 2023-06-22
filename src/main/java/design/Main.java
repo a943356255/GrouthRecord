95f5d2a68d24f8d15aa2d4c2aaa53dae34e6fbb5
a943356255 <943356255@qq.com>
2023-06-22 21:52:19 +0800

    feat: 新增easyexcel依赖，并且支持不用对象导出excel

--- src/main/java/design/Main.java
+++ src/main/java/design/Main.java
@@ -14,4 +14,13 @@ public class Main {
         demo2.templateMethod();
     }
 
+    public void testCallBack(AbstractClass abstractClass) {
+        // 执行其他的业务逻辑
+
+        // 回调函数
+        abstractClass.templateMethod();
+
+        // ...一些其他的业务逻辑
+    }
+
 }
