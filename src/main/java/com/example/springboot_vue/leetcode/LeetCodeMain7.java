96ddc6d65303c2ab10f318f17ac0e2e1ddc2594b
a943356255 <943356255@qq.com>
2023-07-01 10:25:03 +0800

    feat: 1. 两数之和

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain7.java
@@ -8,6 +8,24 @@ public class LeetCodeMain7 {
 
     }
 
+    // 1. 两数之和
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                res[0] = i;
+                res[1] = map.get(nums[i]);
+                break;
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+
+        return res;
+    }
+
     // 2490. 回环句
     public boolean isCircularSentence(String sentence) {
         String[] res = sentence.split(" ");
