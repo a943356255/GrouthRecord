138747613bf0dc4b9c638a7ceef5d0ce4177ed41
a943356255 <943356255@qq.com>
2023-08-05 19:25:16 +0800

    feat: 21. 合并两个有序链表

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain8.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain8.java
@@ -10,6 +10,44 @@ public class LeetCodeMain8 {
         System.out.println(list.get(0));
     }
 
+    // 21. 合并两个有序链表
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode();
+        ListNode temp = new ListNode();
+        res.next = temp;
+        while (list1 != null && list2 != null) {
+            ListNode node = new ListNode();
+            if (list1.val < list2.val) {
+                node.val = list1.val;
+                list1 = list1.next;
+            } else {
+                node.val = list2.val;
+                list2 = list2.next;
+            }
+
+            temp.next = node;
+            temp = temp.next;
+        }
+
+        while (list1 != null) {
+            ListNode node = new ListNode();
+            node.val = list1.val;
+            list1 = list1.next;
+            temp.next = node;
+            temp = temp.next;
+        }
+
+        while (list2 != null) {
+            ListNode node = new ListNode();
+            node.val = list2.val;
+            list2 = list2.next;
+            temp.next = node;
+            temp = temp.next;
+        }
+
+        return res.next.next;
+    }
+
     static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     // 980. 不同路径 III
     public int uniquePathsIII(int[][] grid) {
