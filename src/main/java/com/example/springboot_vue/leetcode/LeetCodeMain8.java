53eda4890ea3eebc79a58a8b163ebd1206926858
a943356255 <943356255@qq.com>
2023-08-06 10:25:52 +0800

    feat: 24. 两两交换链表中的节点

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain8.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain8.java
@@ -10,6 +10,24 @@ public class LeetCodeMain8 {
         System.out.println(list.get(0));
     }
 
+    // 24. 两两交换链表中的节点
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+
+        return dummyHead.next;
+    }
+
     // 21. 合并两个有序链表
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode res = new ListNode();
