c5fa07a3ca2e337191c55c04d231563c0108e2ac
a943356255 <943356255@qq.com>
2023-08-29 10:38:51 +0800

    feat: 823. 带因子的二叉树

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
@@ -1,9 +1,40 @@
 package com.example.springboot_vue.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeetCodeMain9 {
+
+    // 823. 带因子的二叉树
+    public int numFactoredBinaryTrees(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        long[] dp = new long[n];
+        long res = 0, mod = 1000000007;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            // 内层循环每次都是从0到i - 1
+            for (int left = 0, right = i - 1; left <= right; left++) {
+                // 这里是找到第一个满足arr[left] * arr[right]小于当前arr[i]的元素
+                while (right >= left && (long) arr[left] * arr[right] > arr[i]) {
+                    right--;
+                }
+                if (right >= left && (long) arr[left] * arr[right] == arr[i]) {
+                    // 这里，是以di[i]为根节点的二叉树的个数
+                    // 如果左子树不等于右子树，那么两者交换就多了一种情况，如果相等则无法交换，个数就是左子树的种类*右子树种类
+                    if (right != left) {
+                        dp[i] = (dp[i] + dp[left] * dp[right] * 2) % mod;
+                    } else {
+                        dp[i] = (dp[i] + dp[left] * dp[right]) % mod;
+                    }
+                }
+            }
+            res = (res + dp[i]) % mod;
+        }
+
+        return (int) res;
+    }
 
     // 228. 汇总区间
     public List<String> summaryRanges(int[] nums) {
