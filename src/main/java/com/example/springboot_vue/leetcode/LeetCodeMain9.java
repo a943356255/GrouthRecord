baf7bb7f9b27cd628aa823d1588c2ebd8b044084
a943356255 <943356255@qq.com>
2023-09-01 11:06:19 +0800

    feat: 116. 填充每个节点的下一个右侧节点指针

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
@@ -1,10 +1,39 @@
 package com.example.springboot_vue.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class LeetCodeMain9 {
+
+    // 116. 填充每个节点的下一个右侧节点指针
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node temp = queue.poll();
+                if (temp != null) {
+                    if (i < size - 1) {
+                        temp.next = queue.peek();
+                    }
+
+                    if (temp.left != null) {
+                        queue.add(temp.left);
+                    }
+
+                    if (temp.right != null) {
+                        queue.add(temp.right);
+                    }
+                }
+            }
+        }
+
+        return root;
+    }
 
     // 823. 带因子的二叉树
     public int numFactoredBinaryTrees(int[] arr) {
