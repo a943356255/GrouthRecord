d82c771117d27104eb680ee3729765f215b47761
a943356255 <943356255@qq.com>
2023-09-06 10:05:20 +0800

    feat: 1123. 最深叶节点的最近公共祖先

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
@@ -4,6 +4,34 @@ import java.util.*;
 
 public class LeetCodeMain9 {
 
+    private TreeNode ans;
+    private int maxDepth = -1; // 全局最大深度
+    // c
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        dfs(root, 0);
+        return ans;
+    }
+
+    private int dfs(TreeNode node, int depth) {
+        if (node == null) {
+            maxDepth = Math.max(maxDepth, depth); // 维护全局最大深度
+            return depth;
+        }
+
+        // 获取左子树最深叶节点的深度
+        int leftMaxDepth = dfs(node.left, depth + 1);
+        // 获取右子树最深叶节点的深度
+        int rightMaxDepth = dfs(node.right, depth + 1);
+        // 这一步，深度遍历完又回到了根节点，如果他的左子树深度和右子树深度一样，返回改节点即可。
+        // 它会一步一步的回退到最开始的根节点，每次回退都会判断一次
+        if (leftMaxDepth == rightMaxDepth && leftMaxDepth == maxDepth) {
+            ans = node;
+        }
+
+        // 当前子树最深叶节点的深度
+        return Math.max(leftMaxDepth, rightMaxDepth);
+    }
+
     // 2605. 从两个数字数组里生成最小数字
     public int minNumber(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
