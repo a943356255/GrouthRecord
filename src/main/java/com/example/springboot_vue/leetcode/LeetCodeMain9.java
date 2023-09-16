bb45410e7d98920ac783c48c3b79c3aa17907483
a943356255 <943356255@qq.com>
2023-09-16 10:12:53 +0800

    feat: 198. 打家劫舍

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
@@ -11,6 +11,23 @@ public class LeetCodeMain9 {
         }
     }
 
+    // 198. 打家劫舍
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length + 1];
+        Arrays.fill(dp, 0);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+
+        return dp[nums.length];
+    }
+
     // 6. N 字形变换
     public String convert(String s, int numRows) {
         char[][] arr = new char[numRows][s.length()];
