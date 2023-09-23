f49d8da12afed8922d3c9767dab4c8b0676a9947
a943356255 <943356255@qq.com>
2023-09-23 20:36:51 +0800

    feat: 199. 二叉树的右视图

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain9.java
@@ -8,6 +8,36 @@ public class LeetCodeMain9 {
 
     }
 
+    // 199. 二叉树的右视图
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+            }
+        }
+
+        return res;
+    }
+
     // 2591. 将钱分给最多的儿童
     public int distMoney(int money, int children) {
         if (money < children) {
