73bfc9564fece3a306aac2ba34a13c2fcfa979ce
a943356255 <943356255@qq.com>
2023-09-30 10:11:44 +0800

    feat: 2136. 全部开花的最早一天

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain10.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain10.java
@@ -1,5 +1,7 @@
 package com.example.springboot_vue.leetcode;
 
+import java.util.Arrays;
+
 public class LeetCodeMain10 {
 
     public static void main(String[] args) {
@@ -9,6 +11,26 @@ public class LeetCodeMain10 {
     // 394. 字符串解码
     public String decodeString(String s) {
         return null;
+    }
+
+    // 2136. 全部开花的最早一天
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+        int[][] arr = new int[plantTime.length][2];
+        for (int i = 0; i < plantTime.length; i++) {
+            arr[i][0] = plantTime[i];
+            arr[i][1] = growTime[i];
+        }
+        Arrays.sort(arr, (a, b) -> (b[1] - a[1]));
+
+        int[] res = new int[plantTime.length];
+        int max = -1, plant = 0;
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = plant + arr[i][0] + arr[i][1];
+            plant = plant + arr[i][0];
+            max = Math.max(res[i], max);
+        }
+
+        return max;
     }
 
     // 605. 种花问题
