da52bc7d621e36c7acc7058b2b9cf3b82251df03
a943356255 <943356255@qq.com>
2023-10-17 09:32:03 +0800

    feat: 2652. 倍数求和

--- src/main/java/com/example/springboot_vue/leetcode_design/BSTIterator.java
+++ src/main/java/com/example/springboot_vue/leetcode_design/BSTIterator.java
@@ -9,6 +9,17 @@ public class BSTIterator {
     int index = 0, count;
     int[] arr = new int[100001];
 
+    // 2652. 倍数求和
+    public int sumOfMultiples(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                res += i;
+            }
+        }
+        return res;
+    }
+
     public BSTIterator(TreeNode root) {
         this.root = root;
         dfs(root);
