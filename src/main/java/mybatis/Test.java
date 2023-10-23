ca7176842af7475c31692f584066f0bc639541cf
a943356255 <943356255@qq.com>
2023-10-23 10:41:47 +0800

    feat: 新增测试代码

--- src/main/java/mybatis/Test.java
+++ src/main/java/mybatis/Test.java
@@ -1,6 +1,8 @@
 package mybatis;
 
+import com.example.springboot_vue.mapper.crud.CRUDMapper;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -14,6 +16,12 @@ public class Test {
         InputStream inputStream = Resources.getResourceAsStream("");
         // 解析配置文件，并创建工厂类
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        // 创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 创建对应的mapper
+        CRUDMapper crudMapper = sqlSession.getMapper(CRUDMapper.class);
+        // 执行
+        crudMapper.insert(null, null);
     }
 
 }
