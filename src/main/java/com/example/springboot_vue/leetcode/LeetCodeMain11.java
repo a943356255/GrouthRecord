61272f53e70987ca9e8a2a187686419e955a9a3e
a943356255 <943356255@qq.com>
2023-10-24 09:16:19 +0800

    feat: 1155. 掷骰子等于目标和的方法数，纯copy，且没看懂题解

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain11.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain11.java
@@ -19,6 +19,27 @@ public class LeetCodeMain11 {
         testMap.size();
     }
 
+    // 1155. 掷骰子等于目标和的方法数
+    public int numRollsToTarget(int d, int f, int target) {
+        int MOD = 1000000007;
+        int[][] dp = new int[31][1001];
+        int min = Math.min(f, target);
+        for (int i = 1; i <= min; i++) {
+            dp[1][i] = 1;
+        }
+
+        int targetMax = d * f;
+        for (int i = 2; i <= d; i++) {
+            for (int j = i; j <= targetMax; j++) {
+                for (int k = 1; j - k >= 0 && k <= f; k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
+                }
+            }
+        }
+
+        return dp[d][target];
+    }
+
     int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     // 994. 腐烂的橘子
     public int orangesRotting(int[][] grid) {
