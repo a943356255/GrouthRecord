467dd0594584adb4eb750f94b7d465882fb2711a
a943356255 <943356255@qq.com>
2023-10-27 09:08:20 +0800

    feat: 新增测试类，用于写一些测试demo。

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+import cn.dev33.satoken.stp.StpUtil;
+
+public class Test {
+
+    public static void main(String[] args) {
+        StpUtil.login("testId");
+        StpUtil.createLoginSession("testId");
+    }
+
+}
