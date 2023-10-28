49e3b3fb76c0ce1185a55429f3e16e345050d78b
a943356255 <943356255@qq.com>
2023-10-28 15:28:21 +0800

    feat: 新增基于netty的网络编程代码

---
+++ src/main/java/netty/netty/NettyServer.java
@@ -0,0 +1,43 @@
+package netty.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class NettyServer {
+
+    /**
+     *
+     * 这一段代码，实现了NIO模型中服务端的启动，接受新连接，以及打印客户端传来的数据
+     */
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        // 接收新连接线程，创建新的连接
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        // 对应读取数据的线程，用于处理数据读取以及业务逻辑处理
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        serverBootstrap
+                .group(boss, worker)
+                // 这里是接收一个Channel的类型，然后通过反射创建一个工厂类
+                .channel(NioServerSocketChannel.class)
+                // 这个NioSocketChannel是Netty设计的
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new StringDecoder());
+                        nioSocketChannel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+                                System.out.println(s);
+                            }
+                        });
+                    }
+                }).bind(8000);
+    }
+
+}
