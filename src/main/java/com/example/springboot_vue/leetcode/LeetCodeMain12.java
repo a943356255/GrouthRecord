958c4ac9ea2971faf109f98946b38c6a0fbf8fe8
a943356255 <943356255@qq.com>
2023-10-30 20:01:15 +0800

    feat: 31. 下一个排列

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain12.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain12.java
@@ -1,5 +1,7 @@
 package com.example.springboot_vue.leetcode;
 
+import java.util.Arrays;
+
 public class LeetCodeMain12 {
 
     // LCR 077. 排序链表(采用归并排序)
@@ -9,7 +11,40 @@ public class LeetCodeMain12 {
 
     // 31. 下一个排列
     public void nextPermutation(int[] nums) {
+        int first = nums.length - 2, second = nums.length - 1;
+        int mark = 0, minIndex = nums.length - 1;
+        // 这一步结束后，从second ~ num.length - 1必定是降序的
+        while (first >= 0) {
+            if (nums[first] < nums[second]) {
+                mark = 1;
+                break;
+            }
+            first--;
+            second--;
+        }
 
+        if (mark == 0) {
+            Arrays.sort(nums);
+        } else {
+            int tempLeft = second, tempRight = nums.length - 1;
+            while (tempLeft < tempRight) {
+                swap(nums, tempLeft, tempRight);
+                tempLeft++;
+                tempRight--;
+            }
+
+            for (int i = second; i < nums.length; i++) {
+                if (nums[i] > nums[first]) {
+                    swap(nums, i, first);
+                    break;
+                }
+            }
+        }
     }
 
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
