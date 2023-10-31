1c05976da576e2630e2cf869ba9ec8dbccb5ec7d
a943356255 <943356255@qq.com>
2023-10-31 19:08:38 +0800

    feat: 68. 文本左右对齐

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain12.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain12.java
@@ -5,26 +5,81 @@ import java.util.*;
 public class LeetCodeMain12 {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int i = 0; i < 5; i++) {
-            queue.offer(i);
-        }
-        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-        map.put(1, queue);
+//        PriorityQueue<Integer> queue = new PriorityQueue<>();
+//        for (int i = 0; i < 5; i++) {
+//            queue.offer(i);
+//        }
+//        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+//        map.put(1, queue);
+//
+//        PriorityQueue<Integer> temp = new PriorityQueue<>();
+//        temp.addAll(map.get(1));
+//        while (!temp.isEmpty()) {
+//            temp.poll();
+//        }
+//
+//        PriorityQueue<Integer> temp1 = map.get(1);
+//        System.out.println(temp1.size());
 
-        PriorityQueue<Integer> temp = new PriorityQueue<>();
-        temp.addAll(map.get(1));
-        while (!temp.isEmpty()) {
-            temp.poll();
-        }
+        String str = "last    test";
+        String[] result = str.split(" ");
 
-        PriorityQueue<Integer> temp1 = map.get(1);
-        System.out.println(temp1.size());
     }
 
     // LCR 077. 排序链表(采用归并排序)
     public ListNode sortListByMerge(ListNode head) {
         return null;
+    }
+
+    // 68. 文本左右对齐
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int temp = 0, count = 0;
+            // 从i开始，统计往后一行可以插入几个单词
+            for (int j = i; j < words.length; j++) {
+                if (temp + words[j].length() + count <= maxWidth) {
+                    temp += words[j].length();
+                    count++;
+                } else {
+                    break;
+                }
+            }
+
+            int result, spaceLength;
+            if (count == 1) {
+                result = 0;
+                spaceLength = maxWidth - temp;
+            } else {
+                result = (maxWidth - temp) % (count - 1);
+                spaceLength = (maxWidth - temp) / (count - 1);
+            }
+
+            for (int j = i; j < i + count && j < words.length; j++) {
+                stringBuilder.append(words[j]);
+                if (j != i + count - 1 && j != words.length - 1) {
+                    stringBuilder.append(" ".repeat(spaceLength + (result > 0 ? 1 : 0)));
+                }
+                result--;
+            }
+            stringBuilder.append(" ".repeat(Math.max(0, maxWidth - stringBuilder.length())));
+            list.add(stringBuilder.toString());
+            i += count - 1;
+        }
+
+        String[] last = list.get(list.size() - 1).split(" ");
+        StringBuilder lastStr = new StringBuilder();
+        for (String s : last) {
+            if (!s.equals("")) {
+                lastStr.append(s).append(" ");
+            }
+        }
+        lastStr.deleteCharAt(lastStr.length() - 1);
+        lastStr.append(" ".repeat(Math.max(0, maxWidth - lastStr.length())));
+        list.set(list.size() - 1, lastStr.toString());
+
+        return list;
     }
 
     // 2003. 每棵子树内缺失的最小基因值
