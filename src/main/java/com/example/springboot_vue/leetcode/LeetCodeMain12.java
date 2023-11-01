33c389836a1d6c612ebc732d8a4788fecadf0930
a943356255 <943356255@qq.com>
2023-11-01 10:36:58 +0800

    feat: 2127. 参加会议的最多员工数，自己没写出来，copy题解加了注释

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain12.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain12.java
@@ -31,6 +31,102 @@ public class LeetCodeMain12 {
         return null;
     }
 
+    // 2127. 参加会议的最多员工数
+    public int maximumInvitations(int[] favorite) {
+        int n = favorite.length;
+        int[] deg = new int[n];
+        for (int f : favorite) {
+            // 统计基环树每个节点的入度
+            deg[f]++;
+        }
+
+        // 反图,这个反图就是构建了那些入度为0的节点，存储他们指向哪个节点
+        // 后续在查看环为2的时候会用到这里，需要考虑最长的链表
+        List<Integer>[] rg = new List[n];
+        Arrays.setAll(rg, e -> new ArrayList<>());
+
+        // 这里，统计了所有入度为0的节点
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            // deg[i]等于0，说明没有人喜欢他
+            if (deg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        // 拓扑排序，剪掉图上所有树枝
+        // 这里遍历了队列，因为队列中存储了所有入度为0的节点，所以他们不能被邀请
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            // x 只有一条出边
+            int y = favorite[x];
+            rg[y].add(x);
+            // 如果y节点在去除一个指向它的节点后也入度也为0，加入队列
+            // 这里只会去除链表一类的，如果存在环，则没法去除
+            if (--deg[y] == 0) {
+                q.add(y);
+            }
+        }
+
+        int maxRingSize = 0, sumChainSize = 0;
+        for (int i = 0; i < n; i++) {
+            if (deg[i] == 0) continue;
+
+            // 遍历基环上的点
+            // 将基环上的点的入度标记为 0，避免重复访问
+            deg[i] = 0;
+            // 基环长度
+            int ringSize = 1;
+            // 在这里，当有一个i不为0的元素，它一定存在一个环中，所以这里遍历到最后一定会回到i，统计长度就可以
+            for (int x = favorite[i]; x != i; x = favorite[x]) {
+                // 将基环上的点的入度标记为 0，避免重复访问
+                deg[x] = 0;
+                ringSize++;
+            }
+
+            // 基环长度为 2
+            if (ringSize == 2) {
+                // 累加两条最长链的长度
+                sumChainSize += rdfs(i, rg) + rdfs(favorite[i], rg);
+            } else {
+                // 取所有基环长度的最大值
+                maxRingSize = Math.max(maxRingSize, ringSize);
+            }
+        }
+
+        return Math.max(maxRingSize, sumChainSize);
+    }
+
+    // 通过反图 rg 寻找树枝上最深的链
+    private int rdfs(int x, List<Integer>[] rg) {
+        int maxDepth = 1;
+        for (int son : rg[x]) {
+            maxDepth = Math.max(maxDepth, rdfs(son, rg) + 1);
+        }
+
+        return maxDepth;
+    }
+    /**
+     * 想到了去构建一个环，然后环大于2的情况，环的最大大小就是能参加最多的员工
+     * 但是没有想到怎么去构建这个环，想到用链表，但是没法统计每个环的大小
+     * 而且等于2的情况考虑的也有问题，并非简单的+1
+     */
+    public int maximumInvitationsWrong(int[] favorite) {
+        Map<Integer, ListNode> map = new HashMap<>();
+        ListNode head = new ListNode(favorite[0]);
+        int[] arr = new int[favorite.length];
+        Arrays.fill(arr, 0);
+        for (int i = 0; i < favorite.length; i++) {
+            if (arr[i] == 1) {
+                continue;
+            }
+            while (map.get(i) == null) {
+                map.put(i, new ListNode());
+            }
+        }
+        return 0;
+    }
+
     // 68. 文本左右对齐
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> list = new ArrayList<>();
