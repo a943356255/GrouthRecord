6703cef95a685b9794a926e5541b8884435c2f3a
a943356255 <943356255@qq.com>
2023-11-03 10:56:33 +0800

    feat: 新增泛型方法

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,11 +5,18 @@ import cn.dev33.satoken.stp.StpUtil;
 public class Test {
 
     public static void main(String[] args) {
-        StpUtil.login("testId");
-        StpUtil.createLoginSession("testId");
+//        StpUtil.login("testId");
+//        StpUtil.createLoginSession("testId");
+        int a = 1, b = 2;
+        System.out.println(add(a, b));
     }
 
     public void testExe() {
 
+    }
+
+    private static <T extends Number> double add(T a, T b) {
+        System.out.println(a + "+" + b + "=" + (a.doubleValue() + b.doubleValue()));
+        return a.doubleValue() + b.doubleValue();
     }
 }
