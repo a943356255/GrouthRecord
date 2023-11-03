169691b08dbb687f0c4ac96b24cdc057b2d11ed8
a943356255 <943356255@qq.com>
2023-11-03 16:16:31 +0800

    feat: 新增demo

--- src/main/java/generics/Main.java
+++ src/main/java/generics/Main.java
@@ -1,6 +1,8 @@
 package generics;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -22,6 +24,35 @@ public class Main {
 
         Object result =  secondTest.getObject(Class.forName("com.example.springboot_vue.pojo.Demo"));
 
+        List<B> list = new ArrayList<>();
+        funD(list);
+    }
+
+    // 如下两个方法不会报错
+    public static void funA(A a) {
+        // ...
+    }
+    public static void funB(B b) {
+        funA(b);
+        // ...
+    }
+
+    public static void funC(List<? extends A> listA) {
+        // ...
+    }
+
+    public static void funD(List<B> listB) {
+        funC(listB); // OK
+        // ...
     }
 
 }
+
+class A {
+
+}
+
+class B extends A {
+
+}
+
