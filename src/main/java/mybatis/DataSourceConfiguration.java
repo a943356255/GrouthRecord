df7f950b411aeaa204349503fa240e5b03fbf9cf
a943356255 <943356255@qq.com>
2023-11-06 08:50:06 +0800

    feat: 新增利用spi机制，去动态的添加配置文件

---
+++ src/main/java/mybatis/DataSourceConfiguration.java
@@ -0,0 +1,43 @@
+package mybatis;
+
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
+import java.io.File;
+import java.nio.charset.Charset;
+
+//@Configuration
+public class DataSourceConfiguration implements BeanPostProcessor, EnvironmentAware {
+
+    private ConfigurableEnvironment configurableEnvironment;
+
+    @SneakyThrows
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        // 这里就是在Mybatis的Bean初始化之前，我们将读取给定目录下的文件，然后将其塞到配置文件当中，然后用这个值去创建bean
+        if (bean instanceof MybatisAutoConfiguration) {
+            // 这里是读取到的额外的配置文件
+            String config = FileUtils.readFileToString(new File("path"), Charset.defaultCharset());
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.configurableEnvironment = (ConfigurableEnvironment) environment;
+    }
+}
