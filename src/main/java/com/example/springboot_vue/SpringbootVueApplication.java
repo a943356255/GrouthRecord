65d29af75fd28c6c21229966405da39b94b7b007
a943356255 <943356255@qq.com>
2023-11-08 21:04:03 +0800

    feat: 新增注释，解决扫描不到对应包下面方法的问题

--- src/main/java/com/example/springboot_vue/SpringbootVueApplication.java
+++ src/main/java/com/example/springboot_vue/SpringbootVueApplication.java
@@ -3,11 +3,13 @@ package com.example.springboot_vue;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.example.springboot_vue.pojo")
 @MapperScan("com.example.springboot_vue.mapper")
 @MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 @MapperScan("com.example.springboot_vue.file_operate.mapper")
+@ComponentScan(value = "big_file_upload")
 @SpringBootApplication
 public class SpringbootVueApplication {
 
