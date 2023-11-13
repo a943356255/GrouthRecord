3627cdff3c9f1a6d9a092f8c6ecd4dcb0faeabcb
a943356255 <943356255@qq.com>
2023-11-13 19:28:09 +0800

    feat: 新增文件上传至腾讯云存储

---
+++ src/main/java/big_file_upload/upload/tencent/UploadMsg.java
@@ -0,0 +1,18 @@
+package big_file_upload.upload.tencent;
+
+public class UploadMsg {
+
+    public int status;
+    public String msg;
+    public String path;
+
+    public UploadMsg() {
+        super();
+    }
+
+    public UploadMsg (int status, String msg, String path){
+        this.status = status;
+        this.msg = msg;
+        this.path = path;
+    }
+}
