17eab857aa9d709b5ea90c86c2ca7a039528eab6
a943356255 <943356255@qq.com>
2023-11-14 09:28:46 +0800

    feat: 1334. 阈值距离内邻居最少的城市

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain13.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain13.java
@@ -1,4 +1,88 @@
 package com.example.springboot_vue.leetcode;
 
+import java.util.*;
+
 public class LeetCodeMain13 {
+
+    // 1334. 阈值距离内邻居最少的城市
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[] ans = {Integer.MAX_VALUE / 2, -1};
+        int[][] dis = new int[n][n];
+        boolean[][] vis = new boolean[n][n];
+        int[][] mp = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(dis[i], Integer.MAX_VALUE / 2);
+            Arrays.fill(mp[i], Integer.MAX_VALUE / 2);
+        }
+
+        for (int[] eg : edges) {
+            int from = eg[0], to = eg[1], weight = eg[2];
+            mp[from][to] = mp[to][from] = weight;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            dis[i][i] = 0;
+            for (int j = 0; j < n; ++j) {
+                int t = -1;
+                for (int k = 0; k < n; ++k) {
+                    if (!vis[i][k] && (t == -1 || dis[i][k] < dis[i][t])) {
+                        t = k;
+                    }
+                }
+                for (int k = 0; k < n; ++k) {
+                    dis[i][k] = Math.min(dis[i][k], dis[i][t] + mp[t][k]);
+                }
+                vis[i][t] = true;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = 0; j < n; ++j) {
+                if (dis[i][j] <= distanceThreshold) {
+                    cnt++;
+                }
+            }
+            if (cnt <= ans[0]) {
+                ans[0] = cnt;
+                ans[1] = i;
+            }
+        }
+
+        return ans[1];
+//        Map<Integer, List<Map<Integer, Integer>>> map = new HashMap<>();
+//        for (int i = 0; i < edges.length; i++) {
+//            Map<Integer, Integer> partMap = new HashMap<>();
+//            partMap.put(edges[i][1], edges[i][2]);
+//            if (map.get(edges[i][0]) == null) {
+//                List<Map<Integer, Integer>> list = new ArrayList<>();
+//                list.add(partMap);
+//                map.put(edges[i][0], list);
+//            } else {
+//                map.get(edges[i][0]).add(partMap);
+//            }
+//        }
+//
+//        int res = 0;
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        for (int i = 0; i < n; i++) {
+//            queue.add(i);
+//            int[] arr = new int[n];
+//            Arrays.fill(arr, 0);
+//            while (!queue.isEmpty()) {
+//                int size = queue.size();
+//                for (int j = 0; j < size; j++) {
+//                    int node = queue.poll();
+//                    if (arr[node] == 1) {
+//                        continue;
+//                    }
+//                    arr[node] = 1;
+//                    List<Map<Integer, Integer>> list = map.get(node);
+//                }
+//            }
+//        }
+//
+//        return res;
+    }
+
 }
