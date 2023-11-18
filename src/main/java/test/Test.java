073ff3beef6def8060d33b94abfc2d6f6c3ce875
a943356255 <943356255@qq.com>
2023-11-18 10:16:55 +0800

    feat: 新增demo

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -7,16 +7,36 @@ public class Test {
     public static void main(String[] args) {
 //        StpUtil.login("testId");
 //        StpUtil.createLoginSession("testId");
-        int a = 1, b = 2;
-        System.out.println(add(a, b));
+        Test test = new Test();
+        test.testExe();
     }
 
     public void testExe() {
-
+        Father father = new Child();
+        father.fatherMethod();
     }
 
     private static <T extends Number> double add(T a, T b) {
         System.out.println(a + "+" + b + "=" + (a.doubleValue() + b.doubleValue()));
         return a.doubleValue() + b.doubleValue();
+    }
+}
+
+class Father {
+    int test;
+    int name;
+    public void fatherMethod() {
+        System.out.println("456");
+    }
+}
+
+class Child extends Father {
+
+    public void childMethod() {
+
+    }
+
+    public void fatherMethod() {
+        System.out.println("123");
     }
 }
