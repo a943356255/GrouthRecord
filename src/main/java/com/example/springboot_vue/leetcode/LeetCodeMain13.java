e17aac0f4cfad91c956708c4e6f52851a29b8f55
a943356255 <943356255@qq.com>
2023-11-19 09:31:36 +0800

    feat: 三个无重叠子数组组大和

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain13.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain13.java
@@ -4,6 +4,60 @@ import java.util.*;
 
 public class LeetCodeMain13 {
 
+    /**
+     * 我自己的写法用了一个滑动窗口，但实际上三个窗口不交叉的滑动，一次遍历就过了
+     */
+    // 689.三个无重叠子数组组大和
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int[] ans = new int[3];
+        int sum1 = 0, maxSum1 = 0, maxSum1Idx = 0;
+        int sum2 = 0, maxSum12 = 0, maxSum12Idx1 = 0, maxSum12Idx2 = 0;
+        int sum3 = 0, maxTotal = 0;
+        for (int i = k * 2; i < nums.length; ++i) {
+            sum1 += nums[i - k * 2];
+            sum2 += nums[i - k];
+            sum3 += nums[i];
+            if (i >= k * 3 - 1) {
+                if (sum1 > maxSum1) {
+                    maxSum1 = sum1;
+                    maxSum1Idx = i - k * 3 + 1;
+                }
+                if (maxSum1 + sum2 > maxSum12) {
+                    maxSum12 = maxSum1 + sum2;
+                    maxSum12Idx1 = maxSum1Idx;
+                    maxSum12Idx2 = i - k * 2 + 1;
+                }
+                if (maxSum12 + sum3 > maxTotal) {
+                    maxTotal = maxSum12 + sum3;
+                    ans[0] = maxSum12Idx1;
+                    ans[1] = maxSum12Idx2;
+                    ans[2] = i - k + 1;
+                }
+                sum1 -= nums[i - k * 3 + 1];
+                sum2 -= nums[i - k * 2 + 1];
+                sum3 -= nums[i - k + 1];
+            }
+        }
+
+        return ans;
+
+//        int[] res = new int[3];
+//        int[] addSum = new int[nums.length - k + 1];
+//        int index = 0, sum = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (i < k - 1) {
+//                sum += nums[i];
+//                if (i == k - 1) {
+//                    addSum[index] = sum;
+//                }
+//            } else {
+//                index ++;
+//                addSum[index] = addSum[index - 1] + nums[i] - nums[index - 1];
+//            }
+//        }
+//
+//        return res;
+    }
 
     // 2342.数位和相等数对的最大和
     public int maximumSum(int[] nums) {
