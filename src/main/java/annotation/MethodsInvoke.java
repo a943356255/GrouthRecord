b48b3dad311066b44d96d636fe5fb4d3dabca200
a943356255 <943356255@qq.com>
2023-11-22 21:27:46 +0800

    feat: 可以读取注解的版本

--- src/main/java/annotation/MethodsInvoke.java
+++ src/main/java/annotation/MethodsInvoke.java
@@ -1,42 +1,29 @@
 package annotation;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class MethodsInvoke {
 
-//    public void invokeMethods(Class<?> clazz, String methods, Object[] args) throws Exception {
-////        Class<?> c = Class.forName(methods);
-//        Method method = clazz.getMethod(methods);
-//        // 判断对应方法是否有注解
-//        if (!method.isAnnotationPresent(DefaultValue.class)) {
-//            System.out.println("方法没有添加注解");
-//        }
-//
-//        method.invoke(clazz, args);
-//    }
-
-    public void invokeMethods(Object o, String methodName, Object[] args) throws Exception {
+    public void invokeMethods(Object o, String methodName, Class<?>[] args) throws Exception {
         Class<?> clazz = o.getClass();
-
-        Class<?>[] argsClass;
-        if (args != null) {
-            argsClass = new Class[args.length];
-            for (int i = 0; i < args.length; i++) {
-                argsClass[i] = args[i].getClass();
+        Method method = clazz.getMethod(methodName, args);
+        Parameter[] parameters = method.getParameters();
+        int count = 0;
+        for (Parameter parameter : parameters) {
+            if (parameter.isAnnotationPresent(MyDefaultValue.class)) {
+                count++;
             }
-        } else {
-            argsClass = new Class[0];
         }
 
-        Method method = clazz.getMethod(methodName, argsClass);
-
-        // 判断对应方法是否有注解
-        if (!method.isAnnotationPresent(DefaultValue.class)) {
-            System.out.println("方法没有添加注解");
+        Object[] res = new String[count];
+        int index = 0;
+        for (Parameter parameter : parameters) {
+            if (parameter.isAnnotationPresent(MyDefaultValue.class)) {
+                res[index++] = parameter.getAnnotation(MyDefaultValue.class).value();
+            }
         }
 
-        method.invoke(o, args);
+        method.invoke(o, res);
     }
-
 }
