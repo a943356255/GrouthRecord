fec246243651af6e656ac470c2942a34bee64094
a943356255 <943356255@qq.com>
2023-11-27 14:48:54 +0800

    fix: 修复包扫描不到的问题

--- src/main/java/com/example/springboot_vue/SpringbootVueApplication.java
+++ src/main/java/com/example/springboot_vue/SpringbootVueApplication.java
@@ -4,12 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @MapperScan("com.example.springboot_vue.pojo")
 @MapperScan("com.example.springboot_vue.mapper")
 @MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 @MapperScan("com.example.springboot_vue.file_operate.mapper")
-@ComponentScan(value = "big_file_upload")
+@ComponentScans(value = {@ComponentScan(value = "big_file_upload")})
 @SpringBootApplication
 public class SpringbootVueApplication {
 
