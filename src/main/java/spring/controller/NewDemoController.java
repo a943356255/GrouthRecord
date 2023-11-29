a72c5bf3c435ca56f8fc588d3447e8e49bf8034b
a943356255 <943356255@qq.com>
2023-11-29 21:06:51 +0800

    feat: 新增两个demo

--- src/main/java/spring/controller/NewDemoController.java
+++ src/main/java/spring/controller/NewDemoController.java
@@ -1,17 +1,34 @@
 package spring.controller;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import spring.service.TestService;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class NewDemoController {
 
-    @Autowired
+//    @Autowired
+//    TestService testService;
+
+//    @PostConstruct
+//    public void init() {
+//        testService.printf();
+//    }
+
     TestService testService;
 
-    public NewDemoController() {
+    public NewDemoController(TestService testService) {
+        this.testService = testService;
+        System.out.println("执行代码");
         testService.printf();
     }
+
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        testService.printf();
+//    }
 
 }
