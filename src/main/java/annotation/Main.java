55c2fdf2f89a019570b85236a74a11db1dc78d5b
a943356255 <943356255@qq.com>
2023-12-04 08:51:40 +0800

    feat: 新增自定义注解处理器，但是该处理器必须打包成jar，然后被引入才可以使用

--- src/main/java/annotation/Main.java
+++ src/main/java/annotation/Main.java
@@ -3,10 +3,11 @@ package annotation;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        MethodsInvoke methodsInvoke = new MethodsInvoke();
-        Class<?>[] clazz = {String.class, String.class};
-        String[] parameters = {null, "我自己传递的参数"};
-        methodsInvoke.invokeMethods(new TestService(), "testMethods", clazz, parameters);
+//        MethodsInvoke methodsInvoke = new MethodsInvoke();
+//        Class<?>[] clazz = {String.class, String.class};
+//        String[] parameters = {null, "我自己传递的参数"};
+//        methodsInvoke.invokeMethods(new TestService(), "testMethods", clazz, parameters);
+        new TestService().testMethods("first", "second");
     }
 
 }
---
+++ src/main/java/processor/DefaultValueProcess.java
@@ -0,0 +1,36 @@
+package processor;
+
+import annotation.MyDefaultValue;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@SupportedAnnotationTypes({"annotation.MyDefaultValue"})
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
+//@AutoService(Processor.class)
+public class DefaultValueProcess extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        System.out.println("进入该方法");
+        for (TypeElement typeElement : set) {
+            String annotationName = typeElement.getSimpleName().toString();
+            Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(typeElement);
+            for (Element element : elements) {
+                // 获取注解
+                MyDefaultValue myDefaultValue = element.getAnnotation(MyDefaultValue.class);
+                System.out.println(myDefaultValue.value());
+            }
+
+            System.out.println(annotationName);
+            System.out.println(elements.toString());
+        }
+
+        return false;
+    }
+
+
+}
