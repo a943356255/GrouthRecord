92449480db74213a1ee06f15fef636417f324484
a943356255 <943356255@qq.com>
2023-12-04 08:52:19 +0800

    fix: 注释无用代码

--- src/main/java/spring/aop/Application.java
+++ src/main/java/spring/aop/Application.java
@@ -1,32 +1,32 @@
-package spring.aop;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    private OrderService orderService;
-
-    public static void main(String[] args) {
-        new SpringApplication(Application.class).run();
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        SaveOrder saveOrder = new SaveOrder();
-        saveOrder.setSaveId(1L);
-        orderService.saveOrder(saveOrder);
-
-        UpdateOrder updateOrder = new UpdateOrder();
-        updateOrder.setUpdateId(2L);
-        orderService.updateOrder(updateOrder);
-
-    }
-
-
-
-}
+//package spring.aop;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Application implements CommandLineRunner {
+//
+//    @Autowired
+//    private OrderService orderService;
+//
+//    public static void main(String[] args) {
+//        new SpringApplication(Application.class).run();
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        SaveOrder saveOrder = new SaveOrder();
+//        saveOrder.setSaveId(1L);
+//        orderService.saveOrder(saveOrder);
+//
+//        UpdateOrder updateOrder = new UpdateOrder();
+//        updateOrder.setUpdateId(2L);
+//        orderService.updateOrder(updateOrder);
+//
+//    }
+//
+//
+//
+//}
