1dbe71349e9cc44d3b24b5fb70fa20dcc0120901
a943356255 <943356255@qq.com>
2023-12-11 09:41:15 +0800

    feat: 1631. 最小体力消耗路径

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain13.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain13.java
@@ -16,6 +16,45 @@ public class LeetCodeMain13 {
         return res;
     }
 
+    // 1631. 最小体力消耗路径
+    // 是否存在一条从左上角到右下角的路径，其经过的所有边权的最大值不超过 x?
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    // 这里是采用了二分加广搜的办法，二分用于快速限定当前x，而广搜则是判断在绝对值不超过x的时候，是否存在一条路径
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        int left = 0, right = 999999, ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{0, 0});
+            boolean[] seen = new boolean[m * n];
+            seen[0] = true;
+            while (!queue.isEmpty()) {
+                int[] cell = queue.poll();
+                int x = cell[0], y = cell[1];
+                for (int i = 0; i < 4; ++i) {
+                    int nx = x + dirs[i][0];
+                    int ny = y + dirs[i][1];
+                    // 这里不记录值，只判断当前x，y的上下左右4个点的值与当前x和y的差值是否小于等于x，如果是则可以到达，加入队列，否则不可以到达。
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && !seen[nx * n + ny] && Math.abs(heights[x][y] - heights[nx][ny]) <= mid) {
+                        queue.offer(new int[]{nx, ny});
+                        seen[nx * n + ny] = true;
+                    }
+                }
+            }
+            // 这里是直接判断是否可以到达右下角
+            if (seen[m * n - 1]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
     // 70. 爬楼梯
     public int climbStairs(int n) {
         if (n == 1) {
