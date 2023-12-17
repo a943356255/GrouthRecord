04413d22f88c8b9c443d9da97ac6ea05ee7b778e
a943356255 <943356255@qq.com>
2023-12-17 09:29:42 +0800

    feat: 746. 使用最小花费爬楼梯

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
@@ -4,6 +4,18 @@ import java.util.*;
 
 public class LeetCodeMain14 {
 
+    // 746. 使用最小花费爬楼梯
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        dp[cost.length] = Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+        return dp[cost.length];
+    }
+
     // 2415. 反转二叉树的奇数层
     public TreeNode reverseOddLevels(TreeNode root) {
         Queue<TreeNode> queue = new ArrayDeque<>();
