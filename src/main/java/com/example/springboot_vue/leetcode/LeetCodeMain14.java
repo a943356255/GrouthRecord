bd263581c5070867d38929bb3c3a201c682d8b18
a943356255 <943356255@qq.com>
2023-12-22 09:55:11 +0800

    feat: 1671. 得到山形数组的最少删除次数&300. 最长递增子序列

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
@@ -1,7 +1,6 @@
 package com.example.springboot_vue.leetcode;
 
-import org.demo.dependent.Mark;
-import org.demo.dependent.MyDefaultValue;
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.*;
 
@@ -18,6 +17,70 @@ public class LeetCodeMain14 {
             k /= 10;
         }
         System.out.println(strK.compareTo(stringBuilder));
+    }
+
+    // 300. 最长递增子序列
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    res = Math.max(dp[i], res);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    // 1671. 得到山形数组的最少删除次数
+    // 这个题的难点在于，没有考虑好该怎么转化问题，其实就是找到每一个最高点
+    // 然后找他们两端呈山形的最大长度
+    // 相当于计算从左往右和从右往左递增的最大子序列
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        // 这里，得到了每个下标位置，最长的递增子序列
+        int[] pre = getLISArray(nums);
+        // 这三步，是为了得到从右往左递增的序列
+        int[] reversed = reverse(nums);
+        int[] suf = getLISArray(reversed);
+        suf = reverse(suf);
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (pre[i] > 1 && suf[i] > 1) {
+                ans = Math.max(ans, pre[i] + suf[i] - 1);
+            }
+        }
+
+        return n - ans;
+    }
+
+    public int[] getLISArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+
+    public int[] reverse(int[] nums) {
+        int n = nums.length;
+        int[] reversed = new int[n];
+        for (int i = 0; i < n; i++) {
+            reversed[i] = nums[n - 1 - i];
+        }
+        return reversed;
     }
 
     // 2866. 美丽塔 II
