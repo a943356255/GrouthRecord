b55ccd9679d5f395ac835df0c1f664b0b47c9e78
a943356255 <943356255@qq.com>
2023-12-30 14:42:29 +0800

    fix: 增加字段

--- src/main/java/com/example/springboot_vue/pojo/city/City.java
+++ src/main/java/com/example/springboot_vue/pojo/city/City.java
@@ -4,6 +4,42 @@ public class City {
     private String name;
     private String number;
     private String parentName;
+    private String test;
+    private String test2;
+
+    public City() {
+
+    }
+
+    public City(String name, String number, String parentName) {
+        this.name = name;
+        this.number = number;
+        this.parentName = parentName;
+    }
+
+    public City(String name, String number, String parentName, String test, String test2) {
+        this.name = name;
+        this.number = number;
+        this.parentName = parentName;
+        this.test = test;
+        this.test2 = test2;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getTest2() {
+        return test2;
+    }
+
+    public void setTest2(String test2) {
+        this.test2 = test2;
+    }
 
     public String getName() {
         return name;
