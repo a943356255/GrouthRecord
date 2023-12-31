78d4b701ac8e20f7381a4c2385368299cc4eb2b4
a943356255 <943356255@qq.com>
2023-12-31 09:30:34 +0800

    feat: 1154. 一年中的第几天

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
@@ -24,6 +24,24 @@ public class LeetCodeMain14 {
         System.out.println(sum);
     }
 
+    // 1154. 一年中的第几天
+    public int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+
+        int[] amount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            ++amount[1];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < month - 1; ++i) {
+            ans += amount[i];
+        }
+        return ans + day;
+    }
+
     // 1185. 一周中的第几天
     public String dayOfTheWeek(int day, int month, int year) {
         String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
