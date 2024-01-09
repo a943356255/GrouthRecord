d875d8feea816dae39318e6823c86b5bcdffb16c
a943356255 <943356255@qq.com>
2024-01-09 10:28:28 +0800

    feat: 2707. 字符串中的额外字符

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
@@ -24,6 +24,28 @@ public class LeetCodeMain14 {
         System.out.println(sum);
     }
 
+    // 2707. 字符串中的额外字符
+    public int minExtraChar(String s, String[] dictionary) {
+        int n = s.length();
+        int[] d = new int[n + 1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : dictionary) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        d[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (map.containsKey(s.substring(j, i))) {
+                    d[i] = Math.min(d[i], d[j]);
+                }
+            }
+        }
+
+        return d[n];
+    }
+
     // 447. 回旋镖的数量
     public int numberOfBoomerangs(int[][] points) {
         int ans = 0;
