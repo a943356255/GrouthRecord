f8666c394741af423e652c540139879f6cc8b7fa
a943356255 <943356255@qq.com>
2024-01-13 10:17:37 +0800

    frat: 2182. 构造限制重复的字符串

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain14.java
@@ -1,5 +1,7 @@
 package com.example.springboot_vue.leetcode;
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.util.*;
 
 public class LeetCodeMain14 {
@@ -22,6 +24,34 @@ public class LeetCodeMain14 {
             System.out.println(sum);
         }
         System.out.println(sum);
+    }
+
+    // 2182. 构造限制重复的字符串
+    public String repeatLimitedString(String s, int repeatLimit) {
+        int N = 26;
+        int[] count = new int[N];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        StringBuilder ret = new StringBuilder();
+        int m = 0;
+        for (int i = N - 1, j = N - 2; i >= 0 && j >= 0;) {
+            if (count[i] == 0) { // 当前字符已经填完，填入后面的字符，重置 m
+                m = 0;
+                i--;
+            } else if (m < repeatLimit) { // 当前字符未超过限制
+                count[i]--;
+                ret.append((char)('a' + i));
+                m++;
+            } else if (j >= i || count[j] == 0) { // 当前字符已经超过限制，查找可填入的其他字符
+                j--;
+            } else { // 当前字符已经超过限制，填入其他字符，并且重置 m
+                count[j]--;
+                ret.append((char)('a' + j));
+                m = 0;
+            }
+        }
+        return ret.toString();
     }
 
     // 2491. 划分技能点相等的团队
