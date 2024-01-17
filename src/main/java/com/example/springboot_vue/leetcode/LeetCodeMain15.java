c3a4ea4b16f2c64c6da6d14749ed145cb8f74a81
a943356255 <943356255@qq.com>
2024-01-17 09:44:37 +0800

    feat: 2744. 最大字符串配对数目

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -1,4 +1,31 @@
 package com.example.springboot_vue.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCodeMain15 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // 2744. 最大字符串配对数目
+    public int maximumNumberOfStringPairs(String[] words) {
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].charAt(0) != words[i].charAt(words[i].length() - 1))
+                map.put(words[i], "1");
+        }
+
+        int res = 0;
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            StringBuilder temp = new StringBuilder(entry.getKey());
+            if (map.get(temp.reverse().toString()) != null) {
+                res++;
+            }
+        }
+
+        return res / 2;
+    }
+
 }
