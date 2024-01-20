91b075cf9673e0839fc3512fbdbdabe0c0de0054
a943356255 <943356255@qq.com>
2024-01-20 11:58:36 +0800

    feat: 2788. 按分隔符拆分字符串

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -1,13 +1,35 @@
 package com.example.springboot_vue.leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LeetCodeMain15 {
 
     public static void main(String[] args) {
 
+    }
+
+    // 2788. 按分隔符拆分字符串
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> res = new ArrayList<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            int length = word.length();
+            for (int i = 0; i < length; i++) {
+                char c = word.charAt(i);
+                if (c == separator) {
+                    if (sb.length() > 0) {
+                        res.add(sb.toString());
+                        sb.setLength(0);
+                    }
+                } else {
+                    sb.append(c);
+                }
+            }
+            if (sb.length() > 0) {
+                res.add(sb.toString());
+            }
+        }
+        return res;
     }
 
     // 2171. 拿出最少数目的魔法豆
