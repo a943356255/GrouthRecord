ec8f401939e664c470fc5d6833b81f240f511db4
a943356255 <943356255@qq.com>
2024-01-27 18:09:05 +0800

    feat: 2861. 最大合金数

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -10,6 +10,34 @@ public class LeetCodeMain15 {
 
     }
 
+    // 2861. 最大合金数
+    public int maxNumberOfAlloys(int n, int k, int budget, List<List<Integer>> composition, List<Integer> stock, List<Integer> cost) {
+        int left = 1, right = 200000000, ans = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            boolean valid = false;
+            for (int i = 0; i < k; ++i) {
+                long spend = 0;
+                for (int j = 0; j < n; ++j) {
+                    spend += Math.max((long) composition.get(i).get(j) * mid - stock.get(j), 0) * cost.get(j);
+                }
+                if (spend <= budget) {
+                    valid = true;
+                    break;
+                }
+            }
+            if (valid) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
     // 2831. 找出最长等值子数组
     public int longestEqualSubarray(List<Integer> nums, int k) {
         int n = nums.size(), ans = 0;
