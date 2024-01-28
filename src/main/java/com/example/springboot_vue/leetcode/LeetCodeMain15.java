9d167c5f1f064031811569607c20aa3b313961e3
a943356255 <943356255@qq.com>
2024-01-28 17:39:58 +0800

    feat: 365. 水壶问题

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -10,6 +10,43 @@ public class LeetCodeMain15 {
 
     }
 
+    // 365. 水壶问题
+    public boolean canMeasureWater(int x, int y, int z) {
+        Deque<int[]> stack = new LinkedList<int[]>();
+        stack.push(new int[]{0, 0});
+        Set<Long> seen = new HashSet<>();
+        while (!stack.isEmpty()) {
+            if (seen.contains(hash(stack.peek()))) {
+                stack.pop();
+                continue;
+            }
+            seen.add(hash(stack.peek()));
+
+            int[] state = stack.pop();
+            int remain_x = state[0], remain_y = state[1];
+            if (remain_x == z || remain_y == z || remain_x + remain_y == z) {
+                return true;
+            }
+            // 把 X 壶灌满。
+            stack.push(new int[]{x, remain_y});
+            // 把 Y 壶灌满。
+            stack.push(new int[]{remain_x, y});
+            // 把 X 壶倒空。
+            stack.push(new int[]{0, remain_y});
+            // 把 Y 壶倒空。
+            stack.push(new int[]{remain_x, 0});
+            // 把 X 壶的水灌进 Y 壶，直至灌满或倒空。
+            stack.push(new int[]{remain_x - Math.min(remain_x, y - remain_y), remain_y + Math.min(remain_x, y - remain_y)});
+            // 把 Y 壶的水灌进 X 壶，直至灌满或倒空。
+            stack.push(new int[]{remain_x + Math.min(remain_y, x - remain_x), remain_y - Math.min(remain_y, x - remain_x)});
+        }
+        return false;
+    }
+
+    public long hash(int[] state) {
+        return (long) state[0] * 1000001 + state[1];
+    }
+
     // 2861. 最大合金数
     public int maxNumberOfAlloys(int n, int k, int budget, List<List<Integer>> composition, List<Integer> stock, List<Integer> cost) {
         int left = 1, right = 200000000, ans = 0;
