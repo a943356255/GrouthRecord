e3db9cd6eaccd42ff3c015831fc5181f370eddde
a943356255 <943356255@qq.com>
2024-02-02 11:50:00 +0800

    feat: 1686. 石子游戏 VI

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -31,6 +31,34 @@ public class LeetCodeMain15 {
         }
     }
 
+    // 1686. 石子游戏 VI
+    public int stoneGameVI(int[] aliceValues, int[] bobValues) {
+        int n = aliceValues.length;
+        int[][] values = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            values[i][0] = aliceValues[i] + bobValues[i];
+            values[i][1] = aliceValues[i];
+            values[i][2] = bobValues[i];
+        }
+        Arrays.sort(values, (a, b) -> b[0] - a[0]);
+
+        int aliceSum = 0, bobSum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                aliceSum += values[i][1];
+            } else {
+                bobSum += values[i][2];
+            }
+        }
+        if (aliceSum > bobSum) {
+            return 1;
+        } else if (aliceSum == bobSum) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
     // 2670. 找出不同元素数目差数组
     public int[] distinctDifferenceArray(int[] nums) {
         int n = nums.length;
