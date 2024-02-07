0877ec9cf93ea34f5b8242bea61372b514809f9c
a943356255 <943356255@qq.com>
2024-02-07 10:45:28 +0800

    feat: 2641. 二叉树的堂兄弟节点 II

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -31,6 +31,41 @@ public class LeetCodeMain15 {
         }
     }
 
+    // 2641. 二叉树的堂兄弟节点 II
+    public TreeNode replaceValueInTree(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        root.val = 0;
+        while (!queue.isEmpty()) {
+            Queue<TreeNode> queue2 = new ArrayDeque<>();
+            int sum = 0;
+            for (TreeNode fa : queue) {
+                if (fa.left != null) {
+                    queue2.offer(fa.left);
+                    sum += fa.left.val;
+                }
+                if (fa.right != null) {
+                    queue2.offer(fa.right);
+                    sum += fa.right.val;
+                }
+            }
+
+            for (TreeNode fa : queue) {
+                int childSum = (fa.left != null ? fa.left.val : 0) +
+                        (fa.right != null ? fa.right.val : 0);
+                if (fa.left != null) {
+                    fa.left.val = sum - childSum;
+                }
+                if (fa.right != null) {
+                    fa.right.val = sum - childSum;
+                }
+            }
+            queue = queue2;
+        }
+
+        return root;
+    }
+
     // LCP 30. 魔塔游戏
     public int magicTower(int[] nums) {
         // 存放无脑移除的元素
