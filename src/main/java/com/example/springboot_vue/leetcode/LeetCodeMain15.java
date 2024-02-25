269d55f2b6ee2ec771740a09a7f01670713dd59a
a943356255 <943356255@qq.com>
2024-02-25 09:20:51 +0800

    feat: 235. 二叉搜索树的最近公共祖先,注意这是一个二叉搜索树，两次遍历就是记录每一次找到节点p和节点q的路径，然后再遍历路径找到最近的公共祖先

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -29,6 +29,36 @@ public class LeetCodeMain15 {
         }
     }
 
+    // 235. 二叉搜索树的最近公共祖先
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path_p = getPath(root, p);
+        List<TreeNode> path_q = getPath(root, q);
+        TreeNode ancestor = null;
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            if (path_p.get(i) == path_q.get(i)) {
+                ancestor = path_p.get(i);
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+    public List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new ArrayList<>();
+        TreeNode node = root;
+        while (node != target) {
+            path.add(node);
+            if (target.val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        path.add(node);
+        return path;
+    }
+
     TreeMap<Integer, String> treeMap = new TreeMap<>();
     // 2476. 二叉搜索树最近节点查询
     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
