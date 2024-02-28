8f7d6518f7c9b46b200bfb1d8908fc3297b1618c
a943356255 <943356255@qq.com>
2024-02-28 19:45:32 +0800

    feat: 62. 不同路径 --这是2021.03.15 11:11没有做出来的题

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain15.java
@@ -29,6 +29,24 @@ public class LeetCodeMain15 {
         }
     }
 
+    // 62. 不同路径
+    public int uniquePaths(int m, int n) {
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            arr[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+            }
+        }
+
+        return arr[m - 1][n - 1];
+    }
+
     // 110. 平衡二叉树
     public boolean isBalanced(TreeNode root) {
         if (root == null) {
