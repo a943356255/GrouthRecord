135fda52c27a14f0de3c211136c80a8767fa8cc5
a943356255 <943356255@qq.com>
2024-03-04 17:13:53 +0800

    feat: 594. 最长和谐子序列,这是2021.03.05 16:10没有做出来的题

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain16.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain16.java
@@ -6,6 +6,21 @@ public class LeetCodeMain16 {
 
     public static void main(String[] args) {
         String str = "1 + 199929992992";
+
+        /*
+            默认情况下，初始堆内存大小为：电脑内存大小/64
+            默认情况下，最大堆内存大小为：电脑内存大小/4
+         */
+        //返回 JVM 堆大小
+        long initalMemory = Runtime.getRuntime().totalMemory() / 1024 /1024;
+        //返回 JVM 堆的最大内存
+        long maxMemory = Runtime.getRuntime().maxMemory() / 1024 /1024;
+
+        System.out.println("-Xms : " + initalMemory + "M");
+        System.out.println("-Xmx : " + maxMemory + "M");
+
+        System.out.println("系统内存大小：" + initalMemory * 64 / 1024 + "G");
+        System.out.println("系统内存大小：" + maxMemory * 4 / 1024 + "G");
     }
 
 //    public List<String> wordBreak(String s, List<String> wordDict) {
@@ -39,6 +54,37 @@ public class LeetCodeMain16 {
     public int longestSubstring(String s, int k) {
 
         return 0;
+    }
+
+    // 594. 最长和谐子序列
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0, last = nums[0], lastLength = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == last) {
+                lastLength++;
+            } else {
+                if (nums[i] == last + 1) {
+                    int currentLength = 0;
+                    while (nums[i] == last + 1) {
+                        currentLength++;
+                        i++;
+                        if (i == nums.length) {
+                            break;
+                        }
+                    }
+                    i--;
+                    max = Math.max(max, currentLength + lastLength);
+                    lastLength = currentLength;
+                    last = nums[i];
+                } else {
+                    last = nums[i];
+                    lastLength = 1;
+                }
+            }
+        }
+
+        return max;
     }
 
     // 825. 适龄的朋友
