c7ed46da9bce57fb05d71003aeb4e695ec1b97bd
a943356255 <943356255@qq.com>
2024-03-06 09:52:32 +0800

    feat: 2917. 找出数组中的 K-or 值，看不懂题意

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain16.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain16.java
@@ -52,6 +52,23 @@ public class LeetCodeMain16 {
 //        }
 //    }
 
+    // 2917. 找出数组中的 K-or 值
+    public int findKOr(int[] nums, int k) {
+        int ans = 0;
+        for (int i = 0; i < 31; ++i) {
+            int cnt = 0;
+            for (int num : nums) {
+                if (((num >> i) & 1) != 0) {
+                    ++cnt;
+                }
+            }
+            if (cnt >= k) {
+                ans |= 1 << i;
+            }
+        }
+        return ans;
+    }
+
     // BM19 寻找峰值 （牛客）
     public int findPeakElement (int[] nums) {
         if (nums.length == 1) {
