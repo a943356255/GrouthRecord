7532ae861657c340b5849e84591484d73c50627d
a943356255 <943356255@qq.com>
2024-03-07 22:05:53 +0800

    feat: 新增测试用例，用来判断子线程异常是否会结束主线程

--- src/main/java/com/example/springboot_vue/leetcode/CharArray.java
+++ src/main/java/com/example/springboot_vue/leetcode/CharArray.java
@@ -2,6 +2,32 @@ package com.example.springboot_vue.leetcode;
 
 public class CharArray {
 
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+
+            new Thread(() -> {
+                int index = 10;
+                while (index-- > 0) {
+                    System.out.println("第二个线程");
+                }
+
+            }).start();
+
+            new Thread(() -> {
+                int a = 100;
+                int b = 10;
+                int index = 10;
+                while (index > 0) {
+                    a /= (b - index);
+                    index--;
+                }
+            }).start();
+
+            Thread.sleep(1000);
+            System.out.println("我是主线程");
+        }
+    }
+
     char[] element = new char[50];
 
     public CharArray() {
