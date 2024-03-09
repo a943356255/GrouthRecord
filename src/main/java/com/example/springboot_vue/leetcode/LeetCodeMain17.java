7421e4e8d4262769ccf17f4c47665e6ec600722b
a943356255 <943356255@qq.com>
2024-03-09 21:24:36 +0800

    feat: BM63 跳台阶

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain17.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain17.java
@@ -6,4 +6,20 @@ public class LeetCodeMain17 {
 
     }
 
+    // BM63 跳台阶
+    public int jumpFloor (int number) {
+        // write code here
+        if (number < 3) {
+            return number;
+        }
+
+        int[] dp = new int[number + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= number; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[number];
+    }
 }
