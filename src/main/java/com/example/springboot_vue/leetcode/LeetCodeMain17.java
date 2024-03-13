65c9ad38cc092dc9cbf5fc0a980acd3563b1d0fe
a943356255 <943356255@qq.com>
2024-03-13 17:07:50 +0800

    feat: 46. 全排列

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain17.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain17.java
@@ -44,6 +44,66 @@ public class LeetCodeMain17 {
         System.out.println(Arrays.toString(arr));
     }
 
+    // 46. 全排列
+    public List<List<Integer>> permute(int[] nums) {
+        int len = nums.length;
+        // 使用一个动态数组保存所有可能的全排列
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        boolean[] used = new boolean[len];
+        List<Integer> path = new ArrayList<>();
+
+        dfs(nums, len, 0, path, used, res);
+        return res;
+    }
+
+    // 这里是全排列，所以说必须扫描完数组中所有的内容，才可以返回
+    public void permuteDfs(int[] nums, int len, int depth, List<Integer> path, boolean[] used, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(path);
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+                // 这里，depth + 1 和 i + 1的区别
+                permuteDfs(nums, len, depth + 1, path, used, res);
+                used[i] = false;
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private void dfs(int[] nums, int len, int depth, List<Integer> path, boolean[] used, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(path);
+            return;
+        }
+
+        // 在非叶子结点处，产生不同的分支，这一操作的语义是：在还未选择的数中依次选择一个元素作为下一个位置的元素，这显然得通过一个循环实现。
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+
+                dfs(nums, len, depth + 1, path, used, res);
+                // 注意：下面这两行代码发生 「回溯」，回溯发生在从 深层结点 回到 浅层结点 的过程，代码在形式上和递归之前是对称的
+                used[i] = false;
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    // 泛型方法
+    public <T> T getResult(T c) {
+        return c;
+    }
+
     // 142. 环形链表 II
     public ListNode detectCycle(ListNode head) {
         ListNode fast = head, slow = head;
