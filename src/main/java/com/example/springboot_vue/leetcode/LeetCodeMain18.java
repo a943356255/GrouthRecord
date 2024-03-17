25d85121257f629a5d3a07010c608b3565044d80
a943356255 <943356255@qq.com>
2024-03-17 10:42:25 +0800

    feat: 1. 两数之和

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain18.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain18.java
@@ -74,6 +74,22 @@ public class LeetCodeMain18 {
         }
     }
 
+    // 1. 两数之和
+    public int[] twoSumEasy(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = map.get(target - nums[i]);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+        return res;
+    }
+
     // 167. 两数之和 II - 输入有序数组
     public int[] twoSum(int[] numbers, int target) {
         int[] res = new int[2];
