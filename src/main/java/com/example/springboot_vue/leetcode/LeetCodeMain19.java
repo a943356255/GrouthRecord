57ddb59e8b5ada47c617e6559b2b7e1b305012da
a943356255 <943356255@qq.com>
2024-03-20 20:26:50 +0800

    feat: 30. 串联所有单词的子串，有一定的思路，但是代码写起来太复杂了

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
@@ -1,9 +1,47 @@
 package com.example.springboot_vue.leetcode;
 
+import java.util.*;
+
 public class LeetCodeMain19 {
 
     public static void main(String[] args) {
 
+    }
+
+    public int minMutation(String startGene, String endGene, String[] bank) {
+
+    }
+
+    // 30. 串联所有单词的子串
+    public List<Integer> findSubstring(String s, String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.merge(word, 1, Integer::sum);
+        }
+
+        int length = words[0].length();
+        StringBuilder first = new StringBuilder();
+        List<String> list = new ArrayList<>();
+        int left = 0, right = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            first.append(c);
+            if (first.length() == length) {
+                list.add(new String(first));
+                first.delete(left, left + 1);
+                left++;
+            }
+            right++;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        left = 0;
+        right = 0;
+        while (right < list.size()) {
+
+        }
+
+        return res;
     }
 
     // 58. 最后一个单词的长度
