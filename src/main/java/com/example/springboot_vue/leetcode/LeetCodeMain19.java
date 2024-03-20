dd90c264a3d69925de81c805973935c07ca52232
a943356255 <943356255@qq.com>
2024-03-20 18:49:55 +0800

    feat: 58. 最后一个单词的长度

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
@@ -6,4 +6,20 @@ public class LeetCodeMain19 {
 
     }
 
+    // 58. 最后一个单词的长度
+    public int lengthOfLastWord(String s) {
+        int res = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+            } else {
+                while (i >= 0 && s.charAt(i--) != ' ') {
+                    res++;
+                }
+                break;
+            }
+        }
+
+        return res;
+    }
 }
