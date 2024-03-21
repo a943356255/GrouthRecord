404e62d46084bb8aed6ec33dc8898fa98ae5a98b
a943356255 <943356255@qq.com>
2024-03-21 14:53:49 +0800

    feat: LCR 049. 求根节点到叶节点数字之和,手撕，一次过

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
@@ -8,6 +8,55 @@ public class LeetCodeMain19 {
 
     }
 
+    // LCR 049. 求根节点到叶节点数字之和
+    List<List<Integer>> list = new ArrayList<>();
+    public int sumNumbers(TreeNode root) {
+        int res = 0;
+        List<Integer> temp = new ArrayList<>();
+        sumNumbersDfs(root, temp);
+
+        for (int i = 0; i < list.size(); i++) {
+            int tempSum = 0;
+            for (int j = 0; j < list.get(i).size(); j++) {
+                tempSum = tempSum * 10 + list.get(i).get(j);
+            }
+            res += tempSum;
+        }
+
+        return res;
+    }
+
+    public void sumNumbersDfs(TreeNode root, List<Integer> temp) {
+        if (root == null) {
+            return;
+        }
+
+        temp.add(root.val);
+        if (root.left == null && root.right == null) {
+            list.add(new ArrayList<>(temp));
+        }
+        sumNumbersDfs(root.left, temp);
+        sumNumbersDfs(root.right, temp);
+        temp.remove(temp.size() - 1);
+    }
+
+    // 42. 接雨水
+    public int trap(int[] height) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int sum = 0;
+        for (int i = 1; i < height.length; i++) {
+            while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int top = stack.pop();
+
+            }
+
+            stack.push(i);
+        }
+
+        return sum;
+    }
+
     // 14. 最长公共前缀
     public String longestCommonPrefix(String[] strs) {
         StringBuilder res = new StringBuilder();
