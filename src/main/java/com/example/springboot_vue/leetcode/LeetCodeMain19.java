529f1ab8a09ec27bf2f94975fccfb7a80841ecf6
a943356255 <943356255@qq.com>
2024-03-21 09:55:52 +0800

    feat: 14. 最长公共前缀

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
@@ -8,8 +8,47 @@ public class LeetCodeMain19 {
 
     }
 
-    public int minMutation(String startGene, String endGene, String[] bank) {
+    // 14. 最长公共前缀
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder res = new StringBuilder();
 
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < strs.length; i++) {
+            min = Math.min(min, strs[i].length());
+        }
+
+        int right = 0;
+        while (right < min) {
+            char c = strs[0].charAt(right);
+            int same = 1;
+            for (int i = 1; i < strs.length; i++) {
+                if (strs[i].charAt(right) != c) {
+                    same = 0;
+                    break;
+                }
+            }
+            if (same == 1) {
+                res.append(c);
+            } else {
+                break;
+            }
+            right++;
+        }
+
+        return res.toString();
+    }
+
+    // 433. 最小基因变化
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        int res = Integer.MAX_VALUE;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < startGene.length(); i++) {
+            if (startGene.charAt(i) != endGene.charAt(i)) {
+                list.add(i);
+            }
+        }
+
+        return res;
     }
 
     // 30. 串联所有单词的子串
