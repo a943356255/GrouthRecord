374210d33a87b10bb5c2914ea0c6483e0d5cb2fc
a943356255 <943356255@qq.com>
2024-03-26 10:49:59 +0800

    feat: 125. 验证回文串

--- src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
+++ src/main/java/com/example/springboot_vue/leetcode/LeetCodeMain19.java
@@ -47,6 +47,31 @@ public class LeetCodeMain19 {
         test();
     }
 
+    // 125. 验证回文串
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
     // 48. 旋转图像
     public void rotate(int[][] matrix) {
         int length = matrix.length - 1;
